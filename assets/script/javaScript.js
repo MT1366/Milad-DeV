1d2a2001c099d68349420aa10710394e3e9166b5
milad <miladtavassoli13@gmail.com>
2023-05-17 15:50:34 +0330

    some features added to tablet mode and mobile mode, also a simple footer were added and my cv added to its button in aboute me section

--- assets/script/javaScript.js
+++ assets/script/javaScript.js
@@ -157,6 +157,11 @@ let navList = document.querySelector("#navbar");
 let nav = document.querySelectorAll(".navbar li");
 let navClose = document.querySelector(".close-menu");
 let menuBtn = document.querySelector(".button-menu");
+const navChecked = document.querySelector(".navigation__checkbox");
+
+navChecked.addEventListener("click", function () {
+  navigation.classList.toggle(".hidden");
+});
 
 /////////////////// Animatted typing /////////////////////
 
@@ -423,33 +428,3 @@ function animateValue(obj, start, end, duration) {
 document
   .querySelectorAll(".counter")
   .forEach((obj) => animateValue(obj, 0, 423));
-
-// function currentTime() {
-//   let date = new Date();
-//   let hh = date.getHours();
-//   let mm = date.getMinutes();
-//   let ss = date.getSeconds();
-//   let session = "AM";
-
-//   if (hh == 0) {
-//     hh = 12;
-//   }
-//   if (hh > 12) {
-//     hh = hh - 12;
-//     session = "PM";
-//   }
-
-//   hh = hh < 10 ? "0" + hh : hh;
-//   mm = mm < 10 ? "0" + mm : mm;
-//   ss = ss < 10 ? "0" + ss : ss;
-
-//   let time = hh + ":" + mm + ":" + ss + " " + session;
-
-//   document.getElementById("testemonial").innerText = time;
-//   let t = setTimeout(function () {
-//     currentTime();
-//   }, 1000);
-// }
-// currentTime();
-
-// Update Pannel in js
